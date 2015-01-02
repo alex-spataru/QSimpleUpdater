34e331b2ac34413a92d83b35306f6b47cb7925b3
Alex Spataru <alex.racotta@gmail.com>
2015-01-02 14:25:04 -0700

    Minor UI changes

--- QSimpleUpdater/src/qsimpleupdater.cpp
+++ QSimpleUpdater/src/qsimpleupdater.cpp
@@ -10,7 +10,6 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- *
  */
 
 #include "qsimpleupdater.h"
